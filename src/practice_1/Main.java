b15f56fec084656480e4cdf824f3f6ff2b1ccc6d
Vladimir Kuyanov <shuxov_dmitrij@bk.ru>
2024-05-01 15:18:13 +0300

    Added 1 practice

--- src/practice_1/Main.java
+++ src/practice_1/Main.java
@@ -1,7 +1,32 @@
 package practice_1;
 
+import java.util.*;
+import java.util.function.DoubleToIntFunction;
+import java.util.function.Function;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Function<List<Student>, Map<String, List<Student>>> getGroupedStudents = (List<Student> students) -> {
+            Map<String, List<Student>> groups = new HashMap<String, List<Student>>();
+            for (Student student : students) {
+                if (!groups.containsKey(student.group)) {
+                    groups.put(student.group, new ArrayList<Student>());
+                }
+                groups.get(student.group).add(student);
+            }
+            return groups;
+        };
+        
+        ArrayList<Student> students = new ArrayList<Student>();
+        students.add(new Student("Ivanov", "IKBO-01-22"));
+        students.add(new Student("Petrov", "IKBO-02-22"));
+        students.add(new Student("Sokolov", "IKBO-01-22"));
+        Map<String, List<Student>> newmap = getGroupedStudents.apply(students);
+        for (String group : newmap.keySet()) {
+            System.out.println(group + ":");
+            for (Student student : newmap.get(group)) {
+                System.out.println(student.name);
+            }
+        }
     }
 }
---
+++ src/practice_1/Student.java
@@ -0,0 +1,10 @@
+package practice_1;
+
+public class Student {
+    String name;
+    String group;
+    Student(String name, String group) {
+        this.name = name;
+        this.group = group;
+    }
+}
